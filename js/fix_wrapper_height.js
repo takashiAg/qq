632d991c53a1afbfdcda119842611e09ee1699af
takashiAg <crawd4274@gmail.com>
2019-01-12 07:34:37 +0900

    メッセージが少ない時に表示が崩れる問題を直した

---
+++ js/fix_wrapper_height.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function () {
+    setInterval(function () {
+        var h = document.getElementById("content_wrapper").clientHeight
+        var h1 = document.getElementById("sidebar_left").clientHeight
+        var h2 = document.getElementById("sidebar_right").clientHeight
+        var h3 = document.getElementById("main").clientHeight
+        var max = Math.max(h1, h2, h3)
+        document.getElementById("content_wrapper").style.height = (max + 30) + "px"
+    }, 1)
+})
