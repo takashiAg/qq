5c9c1e3de968cd213f36f9d22b630711e00a6e1d
takashiAg <crawd4274@gmail.com>
2019-01-20 04:25:35 +0900

    indexを追加してアクセスした時に見やすくした

---
+++ source/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<ol>
+    <li><a href="/html/QA_top.php">QA_top</a></li>
+    <li><a href="/html/QA_Question.php">QA_question</a></li>
+    <li><a href="/html/QA_questions_list.php">QA_question_list</a></li>
+    <li><a href="/html/QA_category.php">QA_category(template)</a></li>
+    <li><a href="/html/QA_category.php?category=認知症">QA_category(認知症)</a></li>
+    <li><a href="/html/QA_category.php?category=介護保険">QA_category(介護保険)</a></li>
+    <li><a href="/html/QA_category.php?category=要介護認定">QA_category(要介護認定)</a></li>
+    <li><a href="/html/QA_category.php?category=お金">QA_category(お金)</a></li>
+    <li><a href="/html/QA_category.php?category=介護施設">QA_category(介護施設)</a></li>
+    <li><a href="/html/QA_category.php?category=ケアプラン">QA_category(ケアプラン)</a></li>
+    <li><a href="/html/QA_category.php?category=ケアマネージャー">QA_category(ケアマネージャー)</a></li>
+    <li><a href="/html/QA_category.php?category=リハビリ">QA_category(リハビリ)</a></li>
+    <li><a href="/html/QA_category.php?category=リフォーム">QA_category(リフォーム)</a></li>
+    <li><a href="/html/QA_category.php?category=介護器具">QA_category(介護器具)</a></li>
+    <li><a href="/html/QA_category.php?category=介護疲れ・ストレス">QA_category(介護疲れ・ストレス)</a></li>
+    <li><a href="/html/QA_category.php?category=入浴・排泄ケア">QA_category(入浴・排泄ケア)</a></li>
+    <li><a href="/html/QA_category.php?category=家族">QA_category(家族)</a></li>
+    <li><a href="/html/QA_category.php?category=食事">QA_category(食事)</a></li>
+    <li><a href="/html/QA_category.php?category=病気">QA_category(病気)</a></li>
+    <li><a href="/html/QA_category.php?category=元気報告">QA_category(元気報告)</a></li>
+    <li><a href="/html/QA_category.php?category=雑談">QA_category(雑談)</a></li>
+    <li><a href="/html/QA_category.php?category=その他">QA_category(その他)</a></li>
+</ol>
+</body>
+</html>
