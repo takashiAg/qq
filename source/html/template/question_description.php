3462eb32cc8fafc3925e2bcdde9d7660108ef272
takashiAg <crawd4274@gmail.com>
2019-01-21 15:53:49 +0900

    QA_Questionのnoindex/title/metaに対応した

--- source/html/template/question_description.php
+++ source/html/template/question_description.php
@@ -44,7 +44,7 @@ class Contents
         }
 
         if (count($this->question["comments"]) < 5) {
-            $this->noindex = "<meta name=”robots” content=”noindex” />";
+            $this->noindex = "<meta name=\"robots\" content=\"noindex\" />";
         } else {
             $this->noindex = "";
         }
@@ -56,50 +56,6 @@ class Contents
 }
 
 $contents = new Contents();
-//echo count($contents->question["comments"]);
-
-//var_dump($contents)
 ?>
 
-
-<div class="tag"><?php echo $contents->question["tag"]; ?></div>
-<div class="question">
-    <div class="question_wrapper">
-        <div class="padding">
-            <div class="title"><?php echo $contents->question["title"]; ?></div>
-            <div style="margin-top: 10px">
-                <div style="float: left;text-align: left">
-                    <div><span style="color: #ACE698;"><?php echo $contents->question["user"]; ?></span>&nbsp&nbsp<?php echo $contents->question["age"]; ?>才</div>
-                    <div class="target"><?php echo $contents->question["target"]; ?></div>
-                </div>
-                <div style="float: right;margin-right: 20px">
-                    <img src="/img/like.png"><span class="like"><?php echo $contents->question["like"]; ?></span>
-                    <img src="/img/comment.png"><span class="comment"><?php echo count($contents->question["comments"]); ?></span>
-                </div>
-            </div>
-            <div class="clear"></div>
-            <hr color="#DEDEDE">
-
-            <div style="width: 100%;margin-top: 10px">
-                <pre class="message"><?php echo $contents->question["message"]; ?></pre>
-            </div>
-        </div>
-    </div>
-    <img src="/img/icon.png" class="icon">
-    <div class="clear"></div>
-    <div style="float: left">コメント</div>
-    <div class="clear"></div>
-    <?php foreach ($contents->question["comments"] as $key => $commnet) { ?>
-        <div class="comment">
-            <div style="width: 400px;background-color: #FFFFFF;border-radius: 5px;float: left;margin-left: 40px">
-                <div style="color:#ACE698"><?php echo $commnet["tag"]; ?></div>
-                <div><?php echo $commnet["comment"]; ?></div>
-                <div style="float: right;color:#ACE698;border-radius: 7px;border: #ACE698;border-style: solid;border-width: 1px;text-align: center;width: 80px;font-size: 16px;">
-                    共感&nbsp<?php echo $commnet["like"]; ?><img
-                            src="/img/goodjob.png" height="16" width="16"/></div>
-            </div>
-            <img src="/img/icon.png" height="40" width="40" style="float: left;margin: 30px 0px 0px 40px"/>
-        </div>
-    <?php } ?>
-</div>
 
