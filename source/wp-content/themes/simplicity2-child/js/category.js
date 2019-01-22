246f86e03c9462378d349dc27343745a25841b24
takashiAg <crawd4274@gmail.com>
2019-01-23 03:51:04 +0900

    リンクミス

--- source/wp-content/themes/simplicity2-child/js/category.js
+++ source/wp-content/themes/simplicity2-child/js/category.js
@@ -1,26 +1,46 @@
 window.addEventListener("DOMContentLoaded", function () {
-
+    var translate_category = {
+        "お金": "mk_money",
+        "ケアプラン": "mk_care_plan",
+        "ケアマネージャー": "mk_care_manager",
+        "その他": "mk_others",
+        "リハビリ": "mk_rehabilitation",
+        "リフォーム": "mk_reform",
+        "介護保険": "mk_insurance",
+        "介護器具": "mk_device",
+        "介護施設": "mk_-hospital",
+        "介護疲れ・ストレス": "mk_stress",
+        "元気報告": "mk_report",
+        "入浴・排泄ケア": "mk_care",
+        "家族": "mk_family",
+        "病気": "mk_sick",
+        "自己紹介": "mk_self_introduction",
+        "要介護認定": "mk_certification",
+        "認知症": "mk_cognition",
+        "雑談": "mk_chat",
+        "食事": "mk_food"
+    }
     var categories = [
-        {category: "認知症", color: {background: "#DE7171"}, img_source: "/img/category/icon_recognition.png"},
-        {category: "介護保険", color: {background: "#DE71B9"}, img_source: "/img/category/icon_insurance.png"},
-        {category: "要介護認定", color: {background: "#CF71DE"}, img_source: "/img/category/icon_certification.png",},
-        {category: "お金", color: {background: "#DED371"}, img_source: "/img/category/icon_money.png"},
-        {category: "介護施設", color: {background: "#9ADE71"}, img_source: "/img/category/icon_hospital.png"},
-        {category: "ケアプラン", color: {background: "#73DE71"}, img_source: "/img/category/icon_care_plan.png",},
-        {category: "ケアマネージャー", color: {background: "#71DEA3"}, img_source: "/img/category/icon_care_manager.png"},
-        {category: "リハビリ", color: {background: "#71DEDE"}, img_source: "/img/category/icon_rehabilitation.png"},
-        {category: "リフォーム", color: {background: "#71B0DE"}, img_source: "/img/category/icon_reform.png"},
-        {category: "介護器具", color: {background: "#7182DE"}, img_source: "/img/category/icon_device.png"},
-        {category: "介護疲れ・ストレス", color: {background: "#8771DE"}, img_source: "/img/category/icon_stress.png"},
-        {category: "入浴・排泄ケア", color: {background: "#CF71DE"}, img_source: "/img/category/icon_stress.png"},
-        {category: "家族", color: {background: "#DEB271"}, img_source: "/img/category/icon_care.png"},
-        {category: "食事", color: {background: "#D5DE71"}, img_source: "/img/category/icon_family.png"},
-        {category: "病気", color: {background: "#71DEA3"}, img_source: "/img/category/icon_food.png"},
-        {category: "元気報告", color: {background: "#71DEDE"}, img_source: "/img/category/icon_report.png"},
-        {category: "雑談", color: {background: "#87EFEF"}, img_source: "/img/category/icon_chat.png"}
+        {category: "認知症", color: {background: "#DE7171"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_recognition.png"},
+        {category: "介護保険", color: {background: "#DE71B9"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_insurance.png"},
+        {category: "要介護認定", color: {background: "#CF71DE"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_certification.png",},
+        {category: "お金", color: {background: "#DED371"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_money.png"},
+        {category: "介護施設", color: {background: "#9ADE71"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_hospital.png"},
+        {category: "ケアプラン", color: {background: "#73DE71"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_care_plan.png",},
+        {category: "ケアマネージャー", color: {background: "#71DEA3"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_care_manager.png"},
+        {category: "リハビリ", color: {background: "#71DEDE"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_rehabilitation.png"},
+        {category: "リフォーム", color: {background: "#71B0DE"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_reform.png"},
+        {category: "介護器具", color: {background: "#7182DE"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_device.png"},
+        {category: "介護疲れ・ストレス", color: {background: "#8771DE"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_stress.png"},
+        {category: "入浴・排泄ケア", color: {background: "#CF71DE"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_stress.png"},
+        {category: "家族", color: {background: "#DEB271"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_care.png"},
+        {category: "食事", color: {background: "#D5DE71"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_family.png"},
+        {category: "病気", color: {background: "#71DEA3"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_food.png"},
+        {category: "元気報告", color: {background: "#71DEDE"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_report.png"},
+        {category: "雑談", color: {background: "#87EFEF"}, img_source: "https://hajimetenokaigo.jp/wp-content/uploads/2019/01/icon_chat.png"}
     ]
     for (var index  in categories) {
-        categories[index].href = "/html/QA_category.php?category=" + categories[index].category
+        categories[index].href = "/minnadekaigo/" + translate_category[categories[index].category]
     }
     var category = new Vue({
         el: '#category',
