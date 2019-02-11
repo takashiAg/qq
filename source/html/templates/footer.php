3a96c47b5c6d5e49d1cf35ef907103b0540b7c3f
takashiAg <crawd4274@gmail.com>
2019-02-11 23:15:13 +0900

    fix foot er

--- source/html/templates/footer.php
+++ source/html/templates/footer.php
@@ -1,59 +1,129 @@
 <?php
-/**
- * Created by PhpStorm.
- * User=> ryo
- * Date=> 2019-01-18
- * Time=> 11=>55
- */
-
-$footer = array(
-    array(
-        array("name" => "トップページ", "href" => "/top"),
-        array("name" => "カテゴリ一覧", "href" => "/top"),
-        array("name" => "サイトマップ", "href" => "/top"),
-        array("name" => "運営者情報", "href" => "/top"),
-        array("name" => "プライバシーポリシー", "href" => "/top"),
-        array("name" => "利用規約", "href" => "/top"),
-        array("name" => "お問い合わせ", "href" => "/top")
-    ),
-    array(
-        array("name" => "介護が始まるまでの準備", "href" => "/top"),
-        array("name" => "介護が必要になる原因", "href" => "/top"),
-        array("name" => "ケアプラン", "href" => "/top"),
-        array("name" => "ケアマネージャ", "href" => "/top"),
-        array("name" => "介護保険のサービス・施設の種類", "href" => "/top"),
-        array("name" => "国の保険を利用できる被保険者", "href" => "/top"),
-        array("name" => "給付金・保険んりょう・介護費用", "href" => "/top"),
-        array("name" => "要介護認定の申請・更新", "href" => "/top")
-    ),
-    array(
-        array("name" => "リハビリ", "href" => "/top"),
-        array("name" => "介護リフォーム", "href" => "/top"),
-        array("name" => "介護疲れ・ストレス", "href" => "/top"),
-        array("name" => "介護費用の軽減", "href" => "/top"),
-        array("name" => "入浴・清潔ケア", "href" => "/top"),
-        array("name" => "家族での進め方", "href" => "/top"),
-        array("name" => "排泄・トイレ介助", "href" => "/top"),
-        array("name" => "歩行・移乗の介護", "href" => "/top"),
-        array("name" => "福祉用具・介護用品", "href" => "/top"),
-        array("name" => "認知症", "href" => "/top"),
-        array("name" => "食事サポート", "href" => "/top")
-    )
-);
-
-?>
+///**
+// * Created by PhpStorm.
+// * User=> ryo
+// * Date=> 2019-01-18
+// * Time=> 11=>55
+// */
+//
+//$footer = array(
+//    array(
+//        array("name" => "トップページ", "href" => "/"),
+//        array("name" => "カテゴリ一覧", "href" => "/categories"),
+//        array("name" => "サイトマップ", "href" => "/sitemap"),
+//        array("name" => "運営者情報", "href" => "/company"),
+//        array("name" => "プライバシーポリシー", "href" => "/privacy-policy"),
+//        array("name" => "利用規約", "href" => "/terms"),
+//        array("name" => "お問い合わせ", "href" => "/contact")
+//    ),
+//    array(
+//        array("name" => "介護が始まるまでの準備", "href" => "/top"),
+//        array("name" => "介護が必要になる原因", "href" => "/top"),
+//        array("name" => "ケアプラン", "href" => "/top"),
+//        array("name" => "ケアマネージャ", "href" => "/top"),
+//        array("name" => "介護保険のサービス・施設の種類", "href" => "/top"),
+//        array("name" => "国の保険を利用できる被保険者", "href" => "/top"),
+//        array("name" => "給付金・保険んりょう・介護費用", "href" => "/top"),
+//        array("name" => "要介護認定の申請・更新", "href" => "/top")
+//    ),
+//    array(
+//        array("name" => "リハビリ", "href" => "/top"),
+//        array("name" => "介護リフォーム", "href" => "/top"),
+//        array("name" => "介護疲れ・ストレス", "href" => "/top"),
+//        array("name" => "介護費用の軽減", "href" => "/top"),
+//        array("name" => "入浴・清潔ケア", "href" => "/top"),
+//        array("name" => "家族での進め方", "href" => "/top"),
+//        array("name" => "排泄・トイレ介助", "href" => "/top"),
+//        array("name" => "歩行・移乗の介護", "href" => "/top"),
+//        array("name" => "福祉用具・介護用品", "href" => "/top"),
+//        array("name" => "認知症", "href" => "/top"),
+//        array("name" => "食事サポート", "href" => "/top")
+//    )
+//);
+//
+//?>
+<!--<div class="footer-vertical-navi"><p>カテゴリ一覧</p>-->
+<!--    <ul class="footer-category">-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/kaigo-kamo/prepare/">介護が始まるまでの準備</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/kaigo-kamo/sign/">介護が必要になる原因</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/insurance/care-plan/">ケアプラン</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/insurance/care-manager/">ケアマネージャー</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/insurance/services/">介護保険のサービス・施設の種類</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/insurance/target/">国の保険を利用できる被保険者</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/insurance/benefit-money/">給付金・保険料・介護費用</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/insurance/certification/">要介護認定の申請・更新</a></li>-->
+<!--    </ul>-->
+<!--    <ul class="footer-category">-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/rehabilitation/">リハビリ</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/reform/">介護リフォーム</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/fatigue/">介護疲れ・ストレス</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/money/">介護費用の軽減</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/bathe/">入浴・清潔ケア</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/family-how-to/">家族での進め方</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/excretion/">排泄・トイレ介助</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/mobility/">歩行・移乗の介護</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/assistance-instrument/">福祉用具・介護用品</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/dementia-symptoms/">認知症</a></li>-->
+<!--        <li><a href="https://hajimetenokaigo.jp/category/family/meal/">食事サポート</a></li>-->
+<!--    </ul>-->
+<!--</div>-->
+<!--<footer id="footer">-->
+<!--    <div>-->
+<!--        <span>初めての介護について</span>-->
+<!--        <span>カテゴリ一覧</span>-->
+<!--    </div>-->
+<!--    --><?php //foreach ($footer as $k1 => $v1) { ?>
+<!--        <ul>-->
+<!--            --><?php //foreach ($v1 as $k2 => $v2) { ?>
+<!--                <li><a href="--><?php //echo $v2["href"]; ?><!--">--><?php //echo $v2["name"]; ?><!--</a></li>-->
+<!--            --><?php //} ?>
+<!--        </ul>-->
+<!--    --><?php //} ?>
+<!--    <div class="clear center">-->
+<!--        ©︎2018 はじめての介護-->
+<!--    </div>-->
+<!--</footer>-->
 <footer id="footer">
     <div>
         <span>初めての介護について</span>
         <span>カテゴリ一覧</span>
     </div>
-    <?php foreach ($footer as $k1 => $v1) { ?>
-        <ul>
-            <?php foreach ($v1 as $k2 => $v2) { ?>
-                <li><a href="<?php echo $v2["href"]; ?>"><?php echo $v2["name"]; ?></a></li>
-            <?php } ?>
-        </ul>
-    <?php } ?>
+    <ul class="footer-category">
+        <li><a href="https://hajimetenokaigo.jp/category/family/rehabilitation/">リハビリ</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/reform/">介護リフォーム</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/fatigue/">介護疲れ・ストレス</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/money/">介護費用の軽減</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/bathe/">入浴・清潔ケア</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/family-how-to/">家族での進め方</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/excretion/">排泄・トイレ介助</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/mobility/">歩行・移乗の介護</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/assistance-instrument/">福祉用具・介護用品</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/dementia-symptoms/">認知症</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/meal/">食事サポート</a></li>
+    </ul>
+    <ul>
+        <li><a href="https://hajimetenokaigo.jp/category/kaigo-kamo/prepare/">介護が始まるまでの準備</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/kaigo-kamo/sign/">介護が必要になる原因</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/insurance/care-plan/">ケアプラン</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/insurance/care-manager/">ケアマネージャー</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/insurance/services/">介護保険のサービス・施設の種類</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/insurance/target/">国の保険を利用できる被保険者</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/insurance/benefit-money/">給付金・保険料・介護費用</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/insurance/certification/">要介護認定の申請・更新</a></li>
+    </ul>
+    <ul>
+        <li><a href="https://hajimetenokaigo.jp/category/family/rehabilitation/">リハビリ</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/reform/">介護リフォーム</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/fatigue/">介護疲れ・ストレス</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/money/">介護費用の軽減</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/bathe/">入浴・清潔ケア</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/family-how-to/">家族での進め方</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/excretion/">排泄・トイレ介助</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/mobility/">歩行・移乗の介護</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/assistance-instrument/">福祉用具・介護用品</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/dementia-symptoms/">認知症</a></li>
+        <li><a href="https://hajimetenokaigo.jp/category/family/meal/">食事サポート</a></li>
+    </ul>
     <div class="clear center">
         ©︎2018 はじめての介護
     </div>
